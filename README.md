7559164f415fdffd9e4cbb33f9a811d3e5f27861
Botticelli <alizehssn@gmail.com>
2020-08-07 13:31:37 -0500

    Final Project Changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # special-telegram
 Random Password Generator
+//details how you use it //how you did it//purpose//description
--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-// Assignment Code
+/// Assignment Code
+
+//Define Variables
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var digits = "0123456789";
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var symbols = "~'!@#$%^&*()_+{}:?><;.," + '"';
+
+var possibleOptions = "";
+
+var length = 8;
+
+//Generate Password Function Declaration 
+
+function generatePassword() {
+  var passwordOutput = "";
+//Checks for elements to include in Password and store in possibleOption string
+  
+//Loop for the length defined by the user, randomly selecting elements from possibleOption string, storing in passwordFinal
+  for (let i = 0; i < length; i++) {
+    var randomCharacter = Math.floor(Math.random() * possibleOptions.length);
+    passwordOutput = passwordOutput + possibleOptions.charAt(randomCharacter);
+    console.log(passwordOutput);
+  }
+    console.log(passwordOutput);
+    return passwordOutput;
+  }
+  //returns generated password when generate password is called
+  
+
+//Call generatePassword function and store returned value in var password and parse document to display value
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  
+  length = prompt("How many characters(min 8 max 128) do you want your password?");
+  var confirmNumber = confirm("Do you want to include numbers in your password?");
+  if ( confirmNumber=== true) {
+    possibleOptions = possibleOptions.concat(digits);
+    console.log(possibleOptions)
+  }
+  var confirmUpper = confirm("Would you like to include Uppercase Letters?");
+  if (confirmUpper === true) {
+    possibleOptions = possibleOptions.concat(uppercase);
+    console.log(possibleOptions)
+  }
+  var confirmLower = confirm("Would you like to include lowercase letters?");
+  if (confirmLower === true) {
+    possibleOptions = possibleOptions.concat(lowercase);
+    console.log(possibleOptions)
+  }
+  var confirmSymbol = confirm("Would you like to include symbols?");
+  if (confirmSymbol === true) {
+    possibleOptions = possibleOptions.concat(symbols);
+    console.log(possibleOptions)
+  }
+  var password = generatePassword();
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
